592a575e9d7579cc4542f84a115be252d649079e
zxfg223 <zxfg223@gmail.com>
2023-04-18 15:20:45 +0900

    first commit

---
+++ src/main/java/com/example/javaproject2/PrintHello.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2;
+
+public class PrintHello {
+    public void printHello() {
+        System.out.println("Hello");
+    }
+
+
+    public void printBye(){
+        System.out.println("Bye");
+    }
+
+    public void printSum(){
+        System.out.println(1 + 1);
+    }
+}
